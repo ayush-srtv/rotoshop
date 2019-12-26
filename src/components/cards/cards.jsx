20da705ad4e4a4139def58eac5bddcfdb113e2b4
Ayush Srivastava <ayushs.1992@gmail.com>
2019-12-26 22:35:15 +0530

    Added gridlist

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import GridList from "@material-ui/core/GridList";
 
-
-function FilterCards({ cards, skeleton = true }) {
-  return <div />;
+function FilterCards({ cards, skeleton = true, children }) {
+  return <GridList>{children}</GridList>;
 }
 
 export default FilterCards;
