55556118e8ab4d81b0b76c1c5eacff476b4e01ce
Ayush Srivastava <ayushs.1992@gmail.com>
2020-01-09 22:53:38 +0530

    Fixed Tool-tip issue.

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
 import Tooltip from "@material-ui/core/Tooltip";
-import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -15,15 +14,7 @@ const useStyles = makeStyles(theme => ({
 
 function FilterCard({ title, img, subtitle, ...props }) {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
 
-  const handleTooltipClose = () => {
-    setOpen(false);
-  };
-
-  const handleTooltipOpen = () => {
-    setOpen(true);
-  };
   return (
     <GridListTile {...props}>
       <img src={img} alt={title} />
@@ -31,27 +22,14 @@ function FilterCard({ title, img, subtitle, ...props }) {
         title={title}
         subtitle={<span>{subtitle}</span>}
         actionIcon={
-          <ClickAwayListener onClickAway={handleTooltipClose}>
-            <Tooltip
-              PopperProps={{
-                disablePortal: true
-              }}
-              onClose={handleTooltipClose}
-              open={open}
-              disableFocusListener
-              disableHoverListener
-              disableTouchListener
-              title={title}
+          <Tooltip title={title}>
+            <IconButton
+              aria-label={`info about ${title}`}
+              className={classes.icon}
             >
-              <IconButton
-                aria-label={`info about ${title}`}
-                onClick={handleTooltipOpen}
-                className={classes.icon}
-              >
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-          </ClickAwayListener>
+              <InfoIcon />
+            </IconButton>
+          </Tooltip>
         }
       />
     </GridListTile>
