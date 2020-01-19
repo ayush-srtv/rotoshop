8b7bd50547a8d7fe4154900d2e144a5a3a0e3b11
Ayush Srivastava <ayushs.1992@gmail.com>
2020-01-19 23:47:48 +0530

    Moved Filters to config file.

--- src/routes/filters/index.js
+++ src/routes/filters/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { ImageContext } from "../../utils/context/image.context";
 import { Card, Cards } from "../../components/cards";
+import { filters } from "../../config/filters.config.json";
 const useStyles = makeStyles(theme => ({
   root: {
     padding: "10px 30px",
@@ -17,117 +18,6 @@ const useStyles = makeStyles(theme => ({
     height: "100%"
   }
 }));
-
-const filters = [
-  {
-    title: "1977",
-    figClass: "_1977"
-  },
-  {
-    title: "Aden",
-    figClass: "aden"
-  },
-  {
-    title: "Brannan",
-    figClass: "brannan"
-  },
-  {
-    title: "Brooklyn",
-    figClass: "brooklyn"
-  },
-  {
-    title: "Clarendon",
-    figClass: "clarendon"
-  },
-  {
-    title: "Earlybird",
-    figClass: "earlybird"
-  },
-  {
-    title: "Gingham",
-    figClass: "gingham"
-  },
-  {
-    title: "Hudson",
-    figClass: "hudson"
-  },
-  {
-    title: "Inkwell",
-    figClass: "inkwell"
-  },
-  {
-    title: "Kelvin",
-    figClass: "kelvin"
-  },
-  {
-    title: "Lark",
-    figClass: "lark"
-  },
-  {
-    title: "Lo-Fi",
-    figClass: "lofi"
-  },
-  {
-    title: "Maven",
-    figClass: "maven"
-  },
-  {
-    title: "Mayfair",
-    figClass: "mayfair"
-  },
-  {
-    title: "Moon",
-    figClass: "moon"
-  },
-  {
-    title: "Nashville",
-    figClass: "nashville"
-  },
-  {
-    title: "Perpetua",
-    figClass: "perpetua"
-  },
-  {
-    title: "Nashville",
-    figClass: "nashville"
-  },
-  {
-    title: "Reyes",
-    figClass: "reyes"
-  },
-  {
-    title: "Rise",
-    figClass: "rise"
-  },
-  {
-    title: "Slumber",
-    figClass: "slumber"
-  },
-  {
-    title: "Stinson",
-    figClass: "stinson"
-  },
-  {
-    title: "Toaster",
-    figClass: "toaster"
-  },
-  {
-    title: "Valencia",
-    figClass: "valencia"
-  },
-  {
-    title: "Walden",
-    figClass: "walden"
-  },
-  {
-    title: "Willow",
-    figClass: "willow"
-  },
-  {
-    title: "X-pro II",
-    figClass: "xpro2"
-  }
-];
 
 function Filter() {
   const classes = useStyles();
