7a1c691f6489721e53a692423a73de16ff1f3b1f
Ayush Srivastava <ayushs.1992@gmail.com>
2020-01-25 22:38:35 +0530

    Changed material-ui version, added slider.

--- src/routes/settings/index.js
+++ src/routes/settings/index.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+    height: "calc(100% - 132px)"
+  }
+});
 
 function Settings() {
-  return <div>Settings</div>;
+  const classes = useStyles();
+  const [state, setState] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setState(newValue);
+  };
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Slider
+            value={state}
+            onChange={handleChange}
+            aria-labelledby="continuous-slider"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Settings;
