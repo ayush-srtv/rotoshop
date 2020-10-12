ccfb965c385b844e95afb09fc27ac30c63db50c6
Ayush Srivastava <ayushs.1992@gmail.com>
2020-10-12 21:44:38 +0530

    Updates

---
+++ precache-manifest.9ed886bef22599c84dce95d739198324.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55e83510b0ca13d3c0f4f6e8be1c199",
+    "url": "/rotoshop/index.html"
+  },
+  {
+    "revision": "c12fa88458b8b0f78240",
+    "url": "/rotoshop/static/css/main.8956e674.chunk.css"
+  },
+  {
+    "revision": "89381fc784d5a2efce27",
+    "url": "/rotoshop/static/js/5.1a420730.chunk.js"
+  },
+  {
+    "revision": "94341a1d5ea5479bc2f7",
+    "url": "/rotoshop/static/js/6.7b73b2a4.chunk.js"
+  },
+  {
+    "revision": "cf2d82a8b0b60470a00f",
+    "url": "/rotoshop/static/js/7.3a485738.chunk.js"
+  },
+  {
+    "revision": "07f50e9d62abccb1aca9",
+    "url": "/rotoshop/static/js/canvas.dc3dea94.chunk.js"
+  },
+  {
+    "revision": "76e576c5c7ef480e3bcf",
+    "url": "/rotoshop/static/js/filters.a47502f5.chunk.js"
+  },
+  {
+    "revision": "c12fa88458b8b0f78240",
+    "url": "/rotoshop/static/js/main.ce33ceca.chunk.js"
+  },
+  {
+    "revision": "4c8e159e2a06d76ec406",
+    "url": "/rotoshop/static/js/runtime-main.dc619076.js"
+  },
+  {
+    "revision": "392750018b1a57eec1f4",
+    "url": "/rotoshop/static/js/settings.9841941e.chunk.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rotoshop/precache-manifest.9ed886bef22599c84dce95d739198324.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rotoshop/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
